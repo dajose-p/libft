f230a93a57c6ee0c12900a6a31a8f58464b40900
dani <danijosepereira@hotmail.com>
2025-10-01 20:18:20 +0200

    finished strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: danjose- <danjose-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 19:16:49 by danjose-          #+#    #+#             */
-/*   Updated: 2025/10/01 19:31:10 by danjose-         ###   ########.fr       */
+/*   Updated: 2025/10/01 20:17:10 by danjose-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,25 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*us1;
+	unsigned char	*us2;
 
 	i = 0;
-	while (*s1 && (i < n))
+	us1 = (unsigned char *)s1;
+	us2 = (unsigned char *)s2;
+	while ((*us1 || *us2) && (i < n))
 	{
-		if (*s1 == *s2)
+		if (*us1 == *us2)
 		{
 			s1++;
 			s2++;
 			n--;
 			return (ft_strncmp(s1, s2, n));
 		}
-		if (*s1 < *s2)
+		if (*us1 < *us2)
 			return (-1);
-		if (*s1 > *s2)
+		if (*us1 > *us2)
 			return (1);
 		i++;
 	}
