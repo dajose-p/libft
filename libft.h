d38dda41a7f30cb6659f08cd787ce15992faa181
Daniel Jose Pereira <danjose-@student.42madrid.com>
2025-10-06 16:57:11 +0200

    added + bonus

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: danjose- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/28 22:30:05 by danjose-          #+#    #+#             */
-/*   Updated: 2025/10/06 16:15:30 by danjose-         ###   ########.fr       */
+/*   Updated: 2025/10/06 16:53:01 by danjose-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,8 @@ t_list  *ft_lstnew(void *content);
 void    ft_lstadd_front(t_list **lst, t_list *new);
 int     ft_lstsize(t_list *lst);
 t_list  *ft_lstlast(t_list *lst);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void    ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
